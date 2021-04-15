6047209609c6599927b2ffc4c9959acbdcfeb45f
233313 <233313@nd>
2021-04-15 18:54:06 +0800

    添加ssr订阅死链

--- mqtt-font/src/main.js
+++ mqtt-font/src/main.js
@@ -6,11 +6,11 @@ import VueSocketIO from 'vue-socket.io'
 
 Vue.prototype.$axios = axios
 // 临时使用
-axios.defaults.baseURL = process.env.NODE_ENV !== 'production' ? '/api' : 'http://localhost:8083';
+axios.defaults.baseURL = process.env.NODE_ENV !== 'production' ? '/api' : 'http://big.forcebing.top:8083';
 Vue.config.productionTip = false
 Vue.use(new VueSocketIO({
     debug: true,
-    connection: 'http://localhost:8083/test'
+    connection: 'http://big.forcebing.top:8083/test'
 }))
 Vue.config.productionTip = false
 
